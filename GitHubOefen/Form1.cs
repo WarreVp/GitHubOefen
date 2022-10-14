b44975c64b06364156242276b0710460763f98fc
WarreVp <warre.vanpaemel@sintjozefbrugge.be>
2022-10-14 11:31:02 +0200

    Button String

--- GitHubOefen/Form1.cs
+++ GitHubOefen/Form1.cs
@@ -16,5 +16,10 @@ namespace GitHubOefen
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str;
+        }
     }
 }
