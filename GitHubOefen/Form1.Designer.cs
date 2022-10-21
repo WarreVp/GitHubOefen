c35d516103aff926ddd558373982869ad509732a
FerreVerlinde <Ferre.verlinde@sintjozefbrugge.be>
2022-10-21 11:19:49 +0200

    iets

--- GitHubOefen/Form1.Designer.cs
+++ GitHubOefen/Form1.Designer.cs
@@ -32,11 +32,9 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            //button3
             this.radioButton1 = new System.Windows.Forms.RadioButton();
-
             this.button3 = new System.Windows.Forms.Button();
-            //master
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -75,7 +73,6 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "label1";
             // 
-//button3
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
@@ -87,8 +84,8 @@
             this.radioButton1.Text = "radioButton1";
             this.radioButton1.UseVisualStyleBackColor = true;
             this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
-            //
- //button3
+            // 
+            // button3
             // 
             this.button3.Location = new System.Drawing.Point(314, 293);
             this.button3.Name = "button3";
@@ -96,19 +93,24 @@
             this.button3.TabIndex = 4;
             this.button3.Text = "button4";
             this.button3.UseVisualStyleBackColor = true;
-           // this.button3.Click += new System.EventHandler(this.button3_Click);
-//master
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(389, 224);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-//button3
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.radioButton1);
-
             this.Controls.Add(this.button3);
-//master
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
@@ -130,7 +132,8 @@
         private System.Windows.Forms.RadioButton radioButton1;
 
         private System.Windows.Forms.Button button3;
- //master
+        private System.Windows.Forms.Label label2;
+        //master
     }
 }
 
